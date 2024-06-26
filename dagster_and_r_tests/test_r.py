454ad457ac00a7d9a699f7c81873f99f20872c56
Philip Orlando <phlp.orlando@gmail.com>
2024-06-25 19:16:33 -0700

    Update test_r.py

--- dagster_and_r_tests/test_r.py
+++ dagster_and_r_tests/test_r.py
@@ -1,5 +1,8 @@
 from dagster_and_r.docker_and_r import my_docker_and_r_job
 
 def test_my_docker_and_r_job():
+    """
+    Test calling R job from docker.
+    """
     res = my_docker_and_r_job.execute_in_process()
     assert res.success
