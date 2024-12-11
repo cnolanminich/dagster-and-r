4cf059eeb72e02c3e8b2bb5df2be7e3d9af22d2b
Christian Minich <indyman402@gmail.com>
2024-12-11 16:34:36 -0500

    use dagster as dg

--- dagster_and_r/assets.py
+++ dagster_and_r/assets.py
@@ -1,45 +1,38 @@
 import shutil
 import pandas as pd
+import dagster as dg 
 
-from dagster import (
-    AssetExecutionContext,
-    asset,
-    AssetCheckSpec,
-    MaterializeResult,
-    PipesSubprocessClient,
-    file_relative_path,
-    Field, 
-    String,
-    )
-
-@asset
+# example that runs an R script without modification. R script runs but does not report anything in Dagster other than succes.
+@dg.asset
 def hello_world_r(
-    context: AssetExecutionContext,
-    pipes_subprocess_client: PipesSubprocessClient,
-) -> MaterializeResult:
-    cmd = [shutil.which("Rscript"), file_relative_path(__file__, "./R/hello_world.R")]
+    context: dg.AssetExecutionContext,
+    pipes_subprocess_client: dg.PipesSubprocessClient,
+) -> dg.MaterializeResult:
+    cmd = [shutil.which("Rscript"), dg.file_relative_path(__file__, "./R/hello_world.R")]
     return pipes_subprocess_client.run(
         command=cmd,
         context=context,
     ).get_materialize_result()
 
 
-@asset(
-    config_schema={"output_dir": Field(String, default_value="./data")},
+
+#
+@dg.asset(
+    config_schema={"output_dir": dg.Field(dg.String, default_value="./data")},
     check_specs=[
-        AssetCheckSpec(name="no_missing_sepal_length_check_r", asset="iris_r"),
-        AssetCheckSpec(name="no_missing_sepal_width_check_r", asset="iris_r"),
-        AssetCheckSpec(name="no_missing_petal_length_check_r", asset="iris_r"),
-        AssetCheckSpec(name="no_missing_petal_width_check_r", asset="iris_r"),
-        AssetCheckSpec(name="species_name_check_r", asset="iris_r"),
+        dg.AssetCheckSpec(name="no_missing_sepal_length_check_r", asset="iris_r"),
+        dg.AssetCheckSpec(name="no_missing_sepal_width_check_r", asset="iris_r"),
+        dg.AssetCheckSpec(name="no_missing_petal_length_check_r", asset="iris_r"),
+        dg.AssetCheckSpec(name="no_missing_petal_width_check_r", asset="iris_r"),
+        dg.AssetCheckSpec(name="species_name_check_r", asset="iris_r"),
         ],
     )
 def iris_r(
-    context: AssetExecutionContext,
-    pipes_subprocess_client: PipesSubprocessClient,
-) -> MaterializeResult:
+    context: dg.AssetExecutionContext,
+    pipes_subprocess_client: dg.PipesSubprocessClient,
+) -> dg.MaterializeResult:
     output_dir = context.op_config["output_dir"]
-    cmd = [shutil.which("Rscript"), file_relative_path(__file__, "./R/iris.R")]
+    cmd = [shutil.which("Rscript"), dg.file_relative_path(__file__, "./R/iris.R")]
     return pipes_subprocess_client.run(
         command=cmd,
         context=context,
@@ -50,7 +43,7 @@ def iris_r(
     ).get_materialize_result()
 
 
-@asset(deps=[iris_r])
+@dg.asset(deps=[iris_r])
 def iris_py(context):
     # TODO replace hardcoded output_dir with resource key
     iris = pd.read_csv(f"data/iris.csv")
